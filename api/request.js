c14c14829c18c145687700f172cc9d38e5fb5c5a
ɧσℓσ <paulinsegura79@gmail.com>
2022-11-09 19:25:46 +0100

    fix photo system

--- api/request.js
+++ api/request.js
@@ -28,6 +28,8 @@ exports.send = (path, payload = {}) => {
             }
 
             token = data.token;
+            
+            data.data.accounts[0].profile.photo_svg = await this.get_profil_picture(data.data.accounts[0].profile.photo);
 
             if (data.data.accounts) {
                 const index = accounts.findIndex(object => object.id === data.data.accounts[0].id);
@@ -57,6 +59,37 @@ exports.login = (identifiant, motdepasse) => {
         }).catch(err => reject(err))
     })
 
+}
+
+exports.get_profil_picture = (image_link) => {
+
+    var headers =  {
+        Referer: "https://www.ecoledirecte.com/",
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36",
+    };
+
+    if (!image_link) throw new Error('Parametre manquant.');
+    if (!image_link.startsWith('https:')) image_link = 'https:' + image_link;
+
+    return new Promise(async (resolve, reject) => {
+        try {
+            const res = await fetch(image_link, {
+                method: 'GET',
+                headers: { ...headers},
+                redirect: 'follow',
+            });
+            const buf = await res.buffer();
+            const str =
+                "data:" +
+                res.headers.get("Content-Type") +
+                ";base64," +
+                buf.toString("base64");
+            resolve(str);
+        } catch (e) {
+            reject(e);
+        }
+    });
 }
 
 exports.get_accounts = () => { return accounts }
